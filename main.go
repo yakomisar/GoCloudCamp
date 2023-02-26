08cf05a97cccfd5f2ab2932ae4210e111e526488
Олег Комисаренко <oleg.o.komisarenko@gmail.com>
2023-02-26 22:42:09 +0300

    readme added

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"musicplayer/player"
+	"sync"
 	"time"
 )
 
@@ -26,9 +29,16 @@ type Track struct {
 
 type Playlist struct {
 	trackList *Track
+	isPlaying bool
+	isOnPause bool
+	mutex     sync.Mutex
+	pause     chan struct{}
+	resume    chan struct{}
+	done      chan struct{}
 }
 
 // Management methods
+// Add song to the playlist
 func (p *Playlist) AddSong(s Song) {
 	// Check our track list, if there are any songs
 	if p.trackList == nil {
@@ -46,6 +56,69 @@ func (p *Playlist) AddSong(s Song) {
 	}
 }
 
+// Play method allows user to listen to songs one by one from the tracklist
+func (p *Playlist) Play() {
+	if !p.isPlaying && p.isOnPause {
+		fmt.Println("Resume")
+		p.resume <- struct{}{}
+	} else {
+		if p.trackList != nil {
+			p.isPlaying = true
+			//ticker := time.NewTicker(time.Second * p.trackList.current.Duration)
+			ticker := player.New(time.Second * p.trackList.current.Duration)
+			log.Println("Playing song:", p.trackList.current.Name, " duration:", p.trackList.current.Duration)
+			for {
+				select {
+				case <-ticker.Ticks():
+					fmt.Println("here here")
+					ticker.Stop()
+					go p.Next()
+					return
+				case <-p.pause:
+					//ticker.Stop()
+					fmt.Println("Pause button clicked.")
+					p.isPlaying = false
+					p.isOnPause = true
+					<-p.resume
+					fmt.Println("Play button clicked.")
+					p.isPlaying = true
+					p.isOnPause = false
+				case <-p.done:
+					ticker.Stop()
+					return
+				}
+			}
+		} else {
+			log.Println("Please add song to the playlist.")
+		}
+	}
+}
+
+func (p *Playlist) Pause() {
+	p.mutex.Lock()
+	if p.isPlaying {
+		p.pause <- struct{}{}
+	} else {
+		log.Println("Player is on pause state.")
+	}
+	fmt.Println("Trying to pause")
+	p.mutex.Unlock()
+}
+
+// Next method allows user to play the next one song in the list if applicable
+func (p *Playlist) Next() {
+	log.Println("Button \"next\" has been clicked.")
+	//p.mutex.Lock()
+	//// Плеер не играет и стоит на паузе
+	//if !p.isPlaying && p.isOnPause {
+	//
+	//}
+	//// Плеер играет
+	//
+	//// Плеер не играет и не стоит на паузе
+	//p.mutex.Unlock()
+}
+
 func printSongs(p Playlist) {
 	if p.trackList == nil {
 		log.Println("Playlist is empty.")
@@ -56,23 +129,38 @@ func printSongs(p Playlist) {
 		}
 	}
 }
+
 func main() {
 	song1 := Song{
 		Name:     "A-ha",
-		Duration: 1,
+		Duration: 2,
 	}
 	song2 := Song{
 		Name:     "ABBA",
-		Duration: 2,
+		Duration: 3,
 	}
 	song3 := Song{
 		Name:     "BI-2",
-		Duration: 3,
+		Duration: 2,
 	}
 	playlist := Playlist{}
 	playlist.AddSong(song1)
 	playlist.AddSong(song2)
 	playlist.AddSong(song3)
+	playlist.pause = make(chan struct{}, 1)
+	playlist.resume = make(chan struct{}, 1)
+	playlist.done = make(chan struct{}, 1)
 
 	printSongs(playlist)
+	go playlist.Play()
+	time.Sleep(time.Second * 1)
+	playlist.Pause()
+	//playlist.Pause()
+	//playlist.Pause()
+	time.Sleep(time.Second * 7)
+	go playlist.Play()
+	//go playlist.Pause()
+	//time.Sleep(time.Second * 2)
+	//go playlist.Play()
+	time.Sleep(time.Second * 2)
 }
